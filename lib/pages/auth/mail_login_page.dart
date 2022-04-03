9b327f2d317a8a45712de085db688cb6cfea1ff0
kurogoma939 <kan.sss.939@gmail.com>
2022-04-04 01:55:28 +0900

    ➕ FirebaseAuthの認証エラ〜メッセージを日本語化

--- lib/pages/auth/mail_login_page.dart
+++ lib/pages/auth/mail_login_page.dart
@@ -122,13 +122,40 @@ class _MailLoginPageState extends State<MailLoginPage> {
                     });
 
                     return context.go('/');
-                  } catch (e) {
+                  } on FirebaseAuthException catch (e) {
                     // ログインに失敗した場合
-                    setState(() {
-                      infoText = e.toString().replaceAll(RegExp(r'\[.*\] '), '');
-                    });
+                    String message = '';
+                    // エラーコード別処理
+                    switch (e.code) {
+                      case 'invalid-email':
+                        message = 'メールアドレスが不正です。';
+                        break;
+                      case 'wrong-password':
+                        message = 'パスワードが違います。';
+                        break;
+                      case 'user-disabled':
+                        message = '指定されたユーザーは無効です。';
+                        break;
+                      case 'user-not-found':
+                        message = '指定されたユーザーは存在しません。';
+                        break;
+                      case 'operation-not-allowed':
+                        message = '指定されたユーザーはこの操作を許可していません。';
+                        break;
+                      case 'too-many-requests':
+                        message = '複数回リクエストが発生しました。';
+                        break;
+                      case 'email-already-exists':
+                        message = '指定されたメールアドレスは既に使用されています。';
+                        break;
+                      case 'internal-error':
+                        message = '内部処理エラーが発生しました。';
+                        break;
+                      default:
+                        message = '予期せぬエラーが発生しました。';
+                    }
 
-                    return alertDialog('ログインエラー', infoText, context);
+                    return alertDialog('ログインエラー', message, context);
                   }
                 },
                 child: const Text(
