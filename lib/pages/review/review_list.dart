e0c07666fe9fa939630ddb319d3416ad7468725f
kurogoma939 <kan.sss.939@gmail.com>
2022-04-04 00:14:33 +0900

    ➕ リストのスタイル修正（現在日時追加）

--- lib/pages/review/review_list.dart
+++ lib/pages/review/review_list.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../../components/dialog.dart';
 
@@ -12,6 +13,8 @@ class ReviewListPage extends StatefulWidget {
 }
 
 class _ReviewListPageState extends State<ReviewListPage> {
+  DateFormat dateFormat = DateFormat('yyyy/MM/dd');
+
   String uid = '';
   @override
   void initState() {
@@ -27,7 +30,11 @@ class _ReviewListPageState extends State<ReviewListPage> {
     if (uid == documentUserId) {
       return IconButton(
         onPressed: () async {
-          var result = await confirmDialog('確認ダイアログ', '削除してもよろしいですか？', context);
+          var result = await confirmDialog(
+            '確認ダイアログ',
+            '削除してもよろしいですか？',
+            context,
+          );
           if (result == 1) {
             // 削除
             await FirebaseFirestore.instance.collection('reviewList').doc(documentId).delete();
@@ -42,7 +49,7 @@ class _ReviewListPageState extends State<ReviewListPage> {
     }
   }
 
-  final Stream<QuerySnapshot> _reviewStream = FirebaseFirestore.instance.collection('reviewList').orderBy('id', descending: false).snapshots();
+  final Stream<QuerySnapshot> _reviewStream = FirebaseFirestore.instance.collection('reviewList').orderBy('createAt', descending: true).snapshots();
 
   @override
   Widget build(BuildContext context) {
@@ -72,22 +79,31 @@ class _ReviewListPageState extends State<ReviewListPage> {
             child: ListView(
               children: snapshot.data!.docs.map((DocumentSnapshot document) {
                 Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-                return Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                  margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(color: Colors.black45),
-                  ),
-                  child: ListTile(
-                    leading: Text(data['id'].toString()),
-                    title: Text(
-                      data['itemName'] ?? 'データがありません',
-                      style: const TextStyle(fontSize: 20),
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      child: Text(dateFormat.format(data['createAt'].toDate()).toString()),
                     ),
-                    subtitle: Text(data['content'] ?? 'データがありません'),
-                    trailing: _deleteIcon(document.id, data['uid']),
-                  ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(color: Colors.black45),
+                      ),
+                      child: ListTile(
+                        leading: Text(data['id'].toString()),
+                        title: Text(
+                          data['itemName'] ?? 'データがありません',
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                        subtitle: Text(data['content'] ?? 'データがありません'),
+                        trailing: _deleteIcon(document.id, data['uid']),
+                      ),
+                    ),
+                  ],
                 );
               }).toList(),
             ),
